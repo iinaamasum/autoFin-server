f96856ebc35f8c6f0f88ce940b4111675334adea
iinaamasum <iinaamasum@gmail.com>
2022-05-26 12:23:46 +0600

    db connected

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USR}:${process.env.DB_PASS}@cluster0.gcblq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  await client.connect();
+  console.log('db connected');
+}
+
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
